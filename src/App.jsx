9f96cbb2ebca591e9dd9b105baa54093c24dc9c4
thang241190 <thangnguyen24111990@gmail.com>
2022-05-18 11:40:47 +0300

    Add triangles

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,42 @@ import './App.scss'
 
 function App() {
   return (
-    <div className='wrapper'>
-      <div className='logo'>
-        <img src='/images/logo.png' />
+    <div>
+      <div className='triangle-wrapper triangle-wrapper-1'>
+        <div className='triangle-1'>
+          <div></div>
+        </div>
+        <div className='triangle-2'>
+          <div></div>
+        </div>
       </div>
-      <h2 className='text-large'>
-        Sàn giao dịch Bitmoon đóng cửa để chuyển sang trang mới về blockchain
-      </h2>
-      <h3 className='text-small'>
-        vui lòng liên hệ{' '}
-        <a href='mailto:support@bitmoon.net'>support@bitmoon.net</a> <br /> nếu
-        có bất cứ thắc mắc gì
-      </h3>
-      <h3 className='text-small-2 text-small'>Try the new Bitmoon at</h3>
-      <p>
-        <a className='button' href='https://bitmoon.network' target='_blank'>
-          https://bitmoon.network
-        </a>
-      </p>
+      <div className='triangle-wrapper triangle-wrapper-2'>
+        <div className='triangle-1'>
+          <div></div>
+        </div>
+        <div className='triangle-2'>
+          <div></div>
+        </div>
+      </div>
+      <div className='wrapper'>
+        <div className='logo'>
+          <img src='/images/logo.png' />
+        </div>
+        <h2 className='text-large'>
+          Sàn giao dịch Bitmoon đóng cửa để chuyển sang trang mới về blockchain
+        </h2>
+        <h3 className='text-small'>
+          vui lòng liên hệ{' '}
+          <a href='mailto:support@bitmoon.net'>support@bitmoon.net</a> <br />{' '}
+          nếu có bất cứ thắc mắc gì
+        </h3>
+        <h3 className='text-small-2 text-small'>Try the new Bitmoon at</h3>
+        <p>
+          <a className='button' href='https://bitmoon.network' target='_blank'>
+            https://bitmoon.network
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
